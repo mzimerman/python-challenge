1bb1770dc97246a64e08aa62b8b48307904a84f7
Megan Zimerman <mzimerma@labconco.com>
2020-08-05 15:50:50 -0500

    homework update

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,30 +7,72 @@ budget_data = os.path.join("/Users\mzimerma\Desktop\PythonStuff\python-challenge
 
 # Create variables
 total_months = 0
-total_revenue = 0
-sum_revenue_change = 0
+total_pl = 0
+value = 0
+change = 0
+dates = []
+profits = []
 
-# Open and read CSV
+#Opening and reading the CSV file
 with open(budget_data, newline = "") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter = ",")
 
-    # Skip header row   
+    #Reading the header row
     header = next(csvreader)
-    # print(header)
 
+    #Reading the first row (so that we track the changes properly)
+    first_row = next(csvreader)
+    total_months += 1
+    total_pl += int(first_row[1])
+    value = int(first_row[1])
+    
+    #Going through each row of data after the header & first row 
     for row in csvreader:
-        print(row[1])
+        # Keeping track of the dates
+        dates.append(row[0])
+        
+        # Calculate the change, then add it to list of changes
+        change = int(row[1])-value
+        profits.append(change)
+        value = int(row[1])
+        
+        # Find total # of months in dataset
+        total_months += 1
 
-# Find total # of months in dataset
+        # Find net total amount of Profit/Losses
+        total_pl = total_pl + int(row[1])
 
-# Find net total amount of Profit/Losses
+    # Greatest increase in profits (date/amount)
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = dates[greatest_index]
 
-# Average of changes in Profit/Losses
+    # Greatest decrease in losses (date/amount)
+    greatest_decrease = min(profits)
+    worst_index = profits.index(greatest_decrease)
+    worst_date = dates[worst_index]
 
-# Greatest increase in profits (date/amount) 
+    # Average of changes in Profit/Losses
+    avg_change = sum(profits)/len(profits)
+    
 
-# Greatest decrease in losses (date/amount)
-
-# Display information
-print ("Financial Analysis")
+#Displaying information
+print("Financial Analysis")
 print("---------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(total_pl)}")
+print(f"Average Change: ${str(round(avg_change,2))}")
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+
+#Exporting to .txt file
+output = open("PyBank.txt", "w")
+
+line1 = "Financial Analysis"
+line2 = "---------------------"
+line3 = str(f"Total Months: {str(total_months)}")
+line4 = str(f"Total: ${str(total_pl)}")
+line5 = str(f"Average Change: ${str(round(avg_change,2))}")
+line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
