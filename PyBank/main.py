a0629523ac57dd8317732b4dfbf2ed09cd511eae
Megan Zimerman <mzimerma@labconco.com>
2020-08-10 21:08:45 -0500

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -65,14 +65,12 @@ print(f"Average Change: ${str(round(avg_change,2))}")
 print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
 print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
 
-#Exporting to .txt file
-output = open("Analysis/PyBank.txt", "w")
-
-line1 = "Financial Analysis"
-line2 = "---------------------"
-line3 = str(f"Total Months: {str(total_months)}")
-line4 = str(f"Total: ${str(total_pl)}")
-line5 = str(f"Average Change: ${str(round(avg_change,2))}")
-line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
-line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
-output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
+#Export to .txt file
+with open("Analysis/PyBankl.txt", "w") as analysis:
+    analysis.write("Financial Analysis\n")
+    analysis.write("---------------------\n")
+    analysis.write(f"Total Months: {str(total_months)}\n")
+    analysis.write(f"Total: ${str(total_pl)}")
+    analysis.write(f"Average Change: ${str(round(avg_change,2))}")
+    analysis.write(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+    analysis.write(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
